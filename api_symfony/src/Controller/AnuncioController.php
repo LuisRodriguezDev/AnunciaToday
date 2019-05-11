04e58dae6ef090f17520d7613661bb7ddc7d961e
Maryelis <maryelis.mendez@bitsinglass.com>
2019-05-11 20:43:37 -0300

    Crear codigo para acceder Anuncios de la Base de Datos y servirlos a traves de HTTP

--- api_symfony/src/Controller/AnuncioController.php
+++ api_symfony/src/Controller/AnuncioController.php
@@ -1,21 +1,55 @@
 <?php
+
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use App\Entity\Anuncio;
+use App\Form\AnuncioType;
 
-class AnuncioController
+/**
+ * Anuncio controller.
+ * @Route("/api", name="api_")
+ */
+class AnuncioController extends FOSRestController
 {
     /**
-     * @Route("/anuncios")
+     * Muestra todos los Anuncios.
+     * @Rest\Get("/anuncios")
+     * 
+     * @return Response
      */
-    public function anunciosAction()
+    public function getAnuncioAction()
     {
-        return new JsonResponse([
-            [
-                'titulo' => "Buscamos Programador",
-                'contenido' => "Importante Banco Argentino busca Programador Java para importantes aplicaciones"
-            ]
-        ]);
+        $repository = $this->getDoctrine()->getRepository(Anuncio::class);
+        $anuncios = $repository->findall();
+        return $this->handleView($this->view($anuncios));
+    }
+
+    /**
+     * Crear Anuncio.
+     * @Rest\Post("/anuncios")
+     * 
+     * @return Response
+     */
+    public function postAnuncioAction(Request $request)
+    {
+        $anuncio = new Anuncio();
+        $form = $this->CreateForm(AnuncioType::class, $anuncio);
+
+        $data = json_decode($request->getContent(), true);
+        $form->submit($data);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($anuncio);
+            $em->flush();
+            return $this->handleView($this->view(['status' => 'ok'], Response::HTTP_CREATED));
+        }
+
+        return $this->handleView($this->view($form->getErrors()));
     }
 }
