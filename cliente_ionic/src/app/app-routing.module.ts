75619116da89401881083f7b28ef3e6884cbf068
Maryelis <maryelis.mendez@bitsinglass.com>
2019-05-12 00:26:51 -0300

    Configurar modulo de routing para cargar nuestra pagina de anuncios al iniciar la aplicacion

--- cliente_ionic/src/app/app-routing.module.ts
+++ cliente_ionic/src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: '', redirectTo: 'anuncios', pathMatch: 'full' },
   { path: 'anuncios', loadChildren: './pages/anuncios/anuncios.module#AnunciosPageModule' },
 ];
 
