54a707bb9e4794fdc3791b9682bad41e5f1a8526
Maryelis <maryelis.mendez@bitsinglass.com>
2019-05-11 21:49:09 -0300

    Importar Navbar y mostrarlo en la aplicación

--- cliente-react/src/App.js
+++ cliente-react/src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
+import Navbar from './Navbar';
 import Anuncios from './Anuncios';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <section className="section">
-        <div className="container">
-          <h1 className="title has-text-centered">AnunciaToday</h1>
-          <Anuncios />
-        </div>
-      </section>
+      <div>
+        <Navbar />
+        <section className="section">
+          <div className="container">
+            <h1 className="title has-text-centered">AnunciaToday</h1>
+            <Anuncios />
+          </div>
+        </section>
+      </div>
     )
   }
 }
