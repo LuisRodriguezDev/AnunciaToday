e110fb051af1626c62733275bb847cb4d3096490
Maryelis <maryelis.mendez@bitsinglass.com>
2019-05-11 21:08:45 -0300

    Añadir codigo para acceder a los anuncios de la API y mostrarlos en el DOM

--- cliente-react/src/App.js
+++ cliente-react/src/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hola Mundo</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    datos: []
+  }
+
+  componentDidMount() {
+    const url = "http://localhost:8000/api/anuncios";
+
+    fetch(url)
+      .then(respuesta => respuesta.json())
+      .then(respuesta => {
+        this.setState({
+          datos: respuesta
+        })
+      })
+  }
+
+  render() {
+    const { datos } = this.state;
+
+    const resultado = datos.map((anuncio) => {
+      return (
+        <div key={anuncio.id}>
+          <h1>{anuncio.titulo}</h1>
+          <p>{anuncio.descripcion}</p>
+        </div>
+      )
+    })
+
+    return <div>{resultado}</div>
+  }
 }
 
 export default App;
