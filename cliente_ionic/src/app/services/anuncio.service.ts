64a576172b3c0ec4df6af9527359a400d073e176
Maryelis <maryelis.mendez@bitsinglass.com>
2019-05-12 08:35:43 -0300

    Añadir codigo para conectarnos a nuestra API con HTTP

--- cliente_ionic/src/app/services/anuncio.service.ts
+++ cliente_ionic/src/app/services/anuncio.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnuncioService {
+  url = 'http://localhost:8000/api/anuncios';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+
+  }
+
+  getAnuncios() {
+    return this.http.get(`${this.url}`)
+  }
 }
